82959e07190b80a92f9ad5056ea7fe71779d21a3
SimonsRepositories <simon_h@gmx.ch>
2021-07-14 10:55:33 +0200

    init commit

---
+++ src/main/java/ch/cognizant/tools/absencemanagement/AbsenceManagementApplication.java
@@ -0,0 +1,13 @@
+package ch.cognizant.tools.absencemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbsenceManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbsenceManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/cognizant/tools/absencemanagement/AbsenceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.cognizant.tools.absencemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbsenceManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
